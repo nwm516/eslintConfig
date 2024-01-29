db3807acc8999624d63caf69c2258e09228d609b
NW Miller <36825393+nwm516@users.noreply.github.com>
2024-01-28 17:02:27 -0800

    Add files via upload

---
+++ eslintConfig.js
@@ -0,0 +1,26 @@
+/*
+This code is copied from a previous project,
+which is strictly in place for testing purposes.
+*/
+
+import express from 'express';
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.get('/user/:username', (req, res) => {
+    const username = req.params.username;
+    
+    res.type('text/plain');
+    res.send(`Hello, ${username}`);
+});
+
+app.use((req, res) => {
+    res.type('text/plain');
+    res.status(404);
+    res.send("404 - Not Found");
+});
+
+app.listen(app.get('port'), () => {
+    console.log("Connection has been established.");
+});
